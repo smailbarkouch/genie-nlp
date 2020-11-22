56b3015d6a668e122f6ed545c5ce86435e7ef194
Smail Barkouch <SmailBarkouch1@gmail.com>
2020-11-21 21:43:10 -0500

    made it into a command

--- genie-nlp/src/main.rs
+++ genie-nlp/src/main.rs
@@ -1,11 +1,17 @@
 use crate::genie::Genie;
+use std::env;
 
 mod search;
 mod genie;
 mod choice;
 
 fn main() {
-    Genie::perform_search_query("Are oranges or apple better?");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        Genie::perform_search_query(args[1].as_str());
+    } else {
+        println!("Please provide a question encapsulated in quotations.")
+    }
 }
 
 
